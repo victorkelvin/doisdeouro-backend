cedea4f99dc7da805bc60beb946a06cc25644150
victorkelvin <victorkelvin@gmail.com>
2025-05-08 14:53:32 -0300

    `Added custom pagination class and applied it to AlunoViewSet`

--- apps/academia/views.py
+++ apps/academia/views.py
@@ -5,8 +5,13 @@ from rest_framework.pagination import PageNumberPagination
 from .models import Aluno, Turma, Graduacao, DiaSemana
 from .serializers import AlunoSerializer, GraduacaoSerializer, TurmaSerializer, DiaSemanaSerializer
 
+class CustomPagination(PageNumberPagination):
+    page_size = 150
+
+
 class AlunoViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
+    pagination_class = CustomPagination
 
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
